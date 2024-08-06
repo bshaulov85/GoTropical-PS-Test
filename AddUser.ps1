89e0193c8285faed6429efb2249cbeb00b270efb
bshaulov85 <155017492+bshaulov85@users.noreply.github.com>
2024-08-06 15:41:34 -0400

    Add files via upload
    Associated files will create a local ScriptedUser account and add to Administrators group with expiration of 30 days from time of running. Must have access to all servers in the Servers.txt file

---
+++ AddUser.ps1
@@ -0,0 +1,14 @@
+﻿$ScriptBlock = {
+$PWD = ConvertTo-SecureString -String "Script3dUs3rP@$$" -AsPlainText -Force
+$Name = "ScriptedUser"
+$Description = "Local SysAdmin Account"
+$AcctExpiration = (Get-date).AddDays(30).ToString('yyyy-MM-dd')
+New-LocalUser -Name $Name -FullName $Name -Description $Description -Password $PWD -AccountExpires:$AcctExpiration -PasswordNeverExpires:$True -Disabled:$False -Confirm:$False
+Add-LocalGroupMember -Group "Administrators" -Member "ScriptedUser" -Confirm:$False
+}
+$Servers = Get-Content C:\Users\Administrator\Servers.txt
+ForEach ($Server in $Servers)
+	{
+	$Server
+	Invoke-Command -ComputerName $Server -ScriptBlock $ScriptBlock
+	}
